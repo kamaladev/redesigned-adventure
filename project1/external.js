8eba75845c092034edd03474cf986dd9dc972baf
kamaladev <36602787+kamaladev@users.noreply.github.com>
2018-02-18 22:15:42 +0530

    Add files via upload

---
+++ project1/external.js
@@ -0,0 +1,13 @@
+
+ var fruits={name:"pinapple",color:"yellow",county:"10"};
+ fruits,name;
+ var fruits1={name:"orang",color:"yellow",county:"10"};
+ fruits1.name;
+ var fruits2={name:"grp",color:"yellow",county:"10"};
+ fruits2.name;
+ var fruits3={name:"kssk",color:"jsdnjs",county:"10"};
+ fruits3.name;
+ var fruits4={name:"ughreu",color:"yellow",county:"10"};
+ fruits4.name;
+ var fruits5={name:"sdkmksdn",color:"yellow",county:"10"};
+ fruits5.name;
